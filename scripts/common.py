81ef7e29601675dc225f236dc1203e9eed881c16
falkTX <falktx@falktx.com>
2020-07-30 00:54:05 +0100

    Split stuff from python generate script to common file
    Signed-off-by: falkTX <falktx@falktx.com>

---
+++ scripts/common.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+validmodels = (
+    'DUO',
+    'DUOX',
+    'DWARF',
+)
+
+cmds = dict((model,[]) for model in validmodels)
+cmds.update({'ALL':[]})
+
+cmdcount = dict((model,0) for model in validmodels)
+
+bankfuncs = []
+menus = []
+
+flags = {'ALL':[]}
+
+def read_protocol_file(protofile: str):
+    with open(protofile, 'r') as fh:
+        stopcounter = 0
+
+        while True:
+            line = fh.readline()
+
+            if not line:
+                stopcounter += 1
+                if stopcounter == 10:
+                    break
+            else:
+                stopcounter = 0
+
+            line = line.strip()
+
+            if not line.startswith('#define'):
+                continue
+
+            line = line.replace('#define','',1).strip()
+
+            if line.startswith("CMD_"):
+                for model in validmodels:
+                    if line.startswith("CMD_{}_".format(model)):
+                        cmds[model].append(line)
+                        break
+                else:
+                    cmds['ALL'].append(line)
+
+            elif line.startswith("COMMAND_COUNT_"):
+                model, value = line.replace("COMMAND_COUNT_","",1).split()
+
+                if model not in validmodels:
+                    print("ERROR: invalid model in line", line)
+                    return False
+
+                try:
+                    intvalue = int(value)
+                except:
+                    print("ERROR: invalid value in line", line)
+                    return False
+
+                cmdcount[model] = intvalue
+
+            elif line.startswith("BANK_FUNC_"):
+                bankfuncs.append(line)
+
+            elif line.startswith("MENU_ID_"):
+                menus.append(line)
+
+            elif line.startswith("FLAG_"):
+                flagdata = line.replace('FLAG_','',1).split('_',1)
+                if len(flagdata) != 2:
+                    print("ERROR: incorrect flag syntax (1) in line", line)
+                    return False
+                flagtype, flagdata = flagdata
+                flagdata = flagdata.split(None,1)
+                if len(flagdata) != 2:
+                    print("ERROR: incorrect flag syntax (2) in line", line)
+                    return False
+                if flagtype not in flags:
+                    flags[flagtype] = []
+                flags[flagtype].append(flagdata)
+                flags['ALL'].append((flagtype+'_'+flagdata[0], flagdata[1]))
+
+            else:
+                print("ERROR: unhandled define in line", line)
+                return False
+
+    return True
--- scripts/generate-usage.c.py
+++ scripts/generate-usage.c.py
@@ -9,89 +9,10 @@ if len(sys.argv) < 3:
 protofile = sys.argv[1]
 usagefile = sys.argv[2]
 
-validmodels = (
-    'DUO',
-    'DUOX',
-    'DWARF',
-)
+from common import *
 
-cmds = dict((model,[]) for model in validmodels)
-cmds.update({'ALL':[]})
-
-cmdcount = dict((model,0) for model in validmodels)
-
-bankfuncs = []
-menus = []
-
-flags = {'ALL':[]}
-
-with open(protofile, 'r') as fh:
-    stopcounter = 0
-
-    while True:
-        line = fh.readline()
-
-        if not line:
-            stopcounter += 1
-            if stopcounter == 10:
-                break
-        else:
-            stopcounter = 0
-
-        line = line.strip()
-
-        if not line.startswith('#define'):
-            continue
-
-        line = line.replace('#define','',1).strip()
-
-        if line.startswith("CMD_"):
-            for model in validmodels:
-                if line.startswith("CMD_{}_".format(model)):
-                    cmds[model].append(line)
-                    break
-            else:
-                cmds['ALL'].append(line)
-
-        elif line.startswith("COMMAND_COUNT_"):
-            model, value = line.replace("COMMAND_COUNT_","",1).split()
-
-            if model not in validmodels:
-                print("ERROR: invalid model in line", line)
-                sys.exit(2)
-
-            try:
-                intvalue = int(value)
-            except:
-                print("ERROR: invalid value in line", line)
-                sys.exit(2)
-
-            cmdcount[model] = intvalue
-
-        elif line.startswith("BANK_FUNC_"):
-            bankfuncs.append(line)
-
-        elif line.startswith("MENU_ID_"):
-            menus.append(line)
-
-        elif line.startswith("FLAG_"):
-            flagdata = line.replace('FLAG_','',1).split('_',1)
-            if len(flagdata) != 2:
-                print("ERROR: incorrect flag syntax (1) in line", line)
-                sys.exit(2)
-            flagtype, flagdata = flagdata
-            flagdata = flagdata.split(None,1)
-            if len(flagdata) != 2:
-                print("ERROR: incorrect flag syntax (2) in line", line)
-                sys.exit(2)
-            if flagtype not in flags:
-                flags[flagtype] = []
-            flags[flagtype].append(flagdata)
-            flags['ALL'].append((flagtype+'_'+flagdata[0], flagdata[1]))
-
-        else:
-            print("ERROR: unhandled define in line", line)
-            sys.exit(2)
+if not read_protocol_file(protofile):
+    sys.exit(2)
 
 # tests follow, flag is set if we should exit with error
 error = False
